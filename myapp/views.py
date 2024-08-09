6bd072d7f32074227ef21f11ab707a662caa1a99
Lokesh <lokeshsingh7695@gmail.com>
2024-08-09 08:49:29 +0530

    Initial Commit

--- myapp/views.py
+++ myapp/views.py
@@ -1,64 +1,144 @@
-from django.shortcuts import render,redirect
-from django.contrib import messages
-from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from .models import *
+from .models import Post, Comment, Like
+from .forms import PostForm, CommentForm, UsernameChangeForm
+from django.contrib.auth import update_session_auth_hash
+from django.contrib import messages
+from django.db.models import Q
 
-
-def home(request):
-    return render(request, 'home.html')
-
-def register(request):
+# Signup View
+def signup(request):
     if request.method == 'POST':
-        first_name = request.POST.get('first_name')
-        last_name = request.POST.get('last_name')
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-         
-        # Check if a user with the provided username already exists
-        user = User.objects.filter(username=username)
-         
-        if user.exists():
-            # Display an information message if the username is taken
-            messages.info(request, "Username already taken!")
-            return redirect('/register/')
-         
-        # Create a new User object with the provided information
-        user = User.objects.create_user(
-            first_name=first_name,
-            last_name=last_name,
-            username=username
-        )
-         
-        # Set the user's password and save the user object
-        user.set_password(password)
-        user.save()
-         
-        # Display an information message indicating successful account creation
-        messages.info(request, "Account created Successfully!")
-        
-        
-    return render(request, 'register.html')
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('feed')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
 
-def login(request):
-    if request.method == "POST":
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-         
-        # Check if a user with the provided username exists
-        if not User.objects.filter(username=username).exists():
-            # Display an error message if the username does not exist
-            messages.error(request, 'Invalid Username')
-            return redirect('/login/')
-         
-        # Authenticate the user with the provided username and password
-        user = authenticate(username=username, password=password)
-         
-        if user is None:
-            # Display an error message if authentication fails (invalid password)
-            messages.error(request, "Invalid Password")
-            return redirect('/login/')
-        else:
-            # Log in the user and redirect to the home page upon successful login
-            return redirect('/')
-    return render(request, 'login.html')
+# Login View 
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('feed')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+# Logout View 
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+# View for Logged In User Feed and Logged Out User Feed
+def feed(request):
+    posts = Post.objects.all().order_by('-created_at')
+    if request.user.is_authenticated:
+        return render(request, 'feed.html', {'posts': posts})
+    else:
+        return render(request, 'guest_feed.html', {'posts': posts})
+    
+# View for Creating a Post
+@login_required
+def post_create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+            return redirect('feed')
+    else:
+        form = PostForm()
+    return render(request, 'post_create.html', {'form': form})
+
+# View for Deleting a Post
+@login_required
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk, user=request.user)
+    post.delete()
+    return redirect('feed')
+
+# View for Commenting on a Post
+@login_required
+def comment_create(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.user = request.user
+            comment.post = post
+            comment.save()
+            return redirect('feed')
+    else:
+        form = CommentForm()
+    return render(request, 'comment_create.html', {'form': form, 'post': post})
+
+# View for Deleting a Comment
+@login_required
+def comment_delete(request, pk):
+    comment = get_object_or_404(Comment, pk=pk, user=request.user)
+    comment.delete()
+    return redirect('feed')
+
+# View for Liking a Post
+@login_required
+def like(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    like_obj, created = Like.objects.get_or_create(user=request.user, post=post)
+    if not created:
+        like_obj.delete()
+    like_count = Like.objects.filter(post=post).count()
+    return redirect('feed')
+
+# View for showing a user's profile
+def user_profile(request, username):
+    user = get_object_or_404(User, username=username)
+    posts = Post.objects.filter(user=user)
+    likes = Like.objects.filter(user=user)
+    context = {
+        'profile_user': user,
+        'posts': posts,
+        'likes': likes,
+    }
+    return render(request, 'user_profile.html', context)
+
+# View for showing a other user's profile for logged out users
+def guest_profile(request, username):
+    user = get_object_or_404(User, username=username)
+    posts = Post.objects.filter(user=user)
+    return render(request, 'guest_profile.html', {'profile_user': user, 'posts': posts})
+
+# View for changing a user's username
+@login_required
+def change_username(request):
+    if request.method == 'POST':
+        form = UsernameChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Update session to keep user logged in
+            messages.success(request, 'Your username has been updated.')
+            return redirect('user_profile', user.username)
+    else:
+        form = UsernameChangeForm(instance=request.user)
+    return render(request, 'change_username.html', {'form': form})
+
+# View for searching users
+def search_users(request):
+    query = request.GET.get('q', '')
+    users = User.objects.filter(
+        Q(username__icontains=query) |
+        Q(first_name__icontains=query) |
+        Q(last_name__icontains=query)
+    )
+    return render(request, 'search_users.html', {'users': users, 'query': query})
